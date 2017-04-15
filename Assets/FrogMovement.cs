9e2a3447243cb74a5f88c9cfb1b340595af2c22f
George Codat Tudoran <office@solcreation.ro>
2017-04-15 18:14:28 +0300

    Jumping in the direction of the frog's gaze

--- Assets/FrogMovement.cs
+++ Assets/FrogMovement.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class FrogMovement : MonoBehaviour {
 
-    public Vector3 jumpVector;
+    public float jumpElevationInDegrees = 45;
+    public float jumpSpeedMPS = 5;
 
 	// Use this for initialization
 	void Start () {
@@ -12,7 +13,16 @@ public class FrogMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    if(Input.GetKeyDown(KeyCode.Space))
+        var camera = GetComponentInChildren<Camera>();
+        //camera.transform.forward
+        Debug.DrawLine(transform.position, camera.transform.forward, Color.black);
+        var projectedLookDirection = Vector3.ProjectOnPlane(camera.transform.forward, Vector3.up);
+        Debug.DrawLine(transform.position, projectedLookDirection, Color.blue);
+        var radiansToRotate = Mathf.Deg2Rad * jumpElevationInDegrees;
+        var unnormalizedJumpDirection = Vector3.RotateTowards(projectedLookDirection, Vector3.up, radiansToRotate, 0);
+        var jumpVector = unnormalizedJumpDirection.normalized * jumpSpeedMPS;
+        Debug.DrawLine(transform.position, jumpVector, Color.cyan);
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             GetComponent<Rigidbody>().AddForce(jumpVector, ForceMode.VelocityChange);
         }
