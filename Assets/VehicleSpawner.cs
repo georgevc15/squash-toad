76c04bcf69727d4b8a03b61918ee2f81ca2a79fa
George Codat Tudoran <office@solcreation.ro>
2017-04-27 09:23:57 +0300

    Spawning cars randomly

---
+++ Assets/VehicleSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleSpawner : MonoBehaviour {
+
+    public GameObject prefab;
+
+    public float spawnIntervalMin = 2;
+    public float spawnIntervalMax = 10;
+    float nextSpwanTime = 0;
+    
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time > nextSpwanTime)
+        {
+            Spawn();
+            nextSpwanTime = Time.time + Random.Range(spawnIntervalMin, spawnIntervalMax);
+        }
+	}
+
+
+    void Spawn()
+    {
+        var instance = Instantiate(prefab, transform.position, transform.rotation, transform);
+    }
+
+}
