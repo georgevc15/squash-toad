292cd8ee5d12369422537b202f886a504e42950b
George Codat Tudoran <office@solcreation.ro>
2017-05-15 19:40:28 +0300

    Pausing game mechanics

--- Assets/Death.cs
+++ Assets/Death.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class Death : MonoBehaviour {
 
     public GameObject UICanvas;
+    public GameObject Reticule;
 
     public void OnDeath()
     {
         UICanvas.SetActive(true);
+        Reticule.SetActive(true);
+        GetComponent<Rigidbody>().isKinematic = true;
     }
 
 }
